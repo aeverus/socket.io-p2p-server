a337d006e0c34b6e20e3934f09aa9efccb3c4d49
aeverus <aeverus@users.noreply.github.com>
2018-04-12 14:44:40 +0800

    moved index.js logics to socket.io-p2p

--- index.js
+++ index.js
@@ -1,45 +1,63 @@
 var debug = require('debug')('socket')
-var clients = {}
+var clients = {} // if multi servers, clients only has sockets from its own server
 
 module.exports.clients = clients
 module.exports.Server = p2pSocket
 
-function p2pSocket (socket, next, room) {
-  clients[socket.id] = socket
-  if (typeof room === 'object') {
-    var connectedClients = socket.adapter.rooms[room.name]
-  } else {
-    var connectedClients = clients
-  }
-  socket.emit('numClients', Object.keys(connectedClients).length - 1)
 
-  socket.on('disconnect', function () {
-    delete clients[socket.id]
-    Object.keys(connectedClients).forEach(function (clientId, i) {
-      var client = clients[clientId]
-      client.emit('peer-disconnect', {peerId: socket.id})
-    })
-    debug('Client gone (id=' + socket.id + ').')
-  })
+/**
+* move to server.js
+*/
+function p2pSocket(socket, next, room) {
+    // clients[socket.id] = socket
+    // if (typeof room === 'object') {
+    //     var connectedClients = socket.adapter.rooms[room.name]
+    // } else {
+    //     var connectedClients = clients
+    // }
+    // var connectedClients = clients
+    // socket.emit('numClients', Object.keys(connectedClients).length - 1)
 
-  socket.on('offers', function (data) {
-    // send offers to everyone in a given room
-    Object.keys(connectedClients).forEach(function (clientId, i) {
-      var client = clients[clientId]
-      if (client !== socket) {
-        var offerObj = data.offers[i]
-        var emittedOffer = {fromPeerId: socket.id, offerId: offerObj.offerId, offer: offerObj.offer}
-        debug('Emitting offer: %s', JSON.stringify(emittedOffer))
-        client.emit('offer', emittedOffer)
-      }
-    })
-  })
+    // socket.on('disconnect', function() {
+    //     delete clients[socket.id]
+    //     Object.keys(clients).forEach(function(clientId, i) {
+    //         var client = clients[clientId]
+    //         client.emit('peer-disconnect', {
+    //             peerId: socket.id
+    //         })
+    //     })
+    //     // debug('Client gone (id=' + socket.id + ').')
+    //     console.log('self disconnect [%s]', socket.id)
+    // })
 
-  socket.on('peer-signal', function (data) {
-    var toPeerId = data.toPeerId
-    debug('Signal peer id %s', toPeerId);
-    var client = clients[toPeerId]
-    client.emit('peer-signal', data)
-  })
-  typeof next === 'function' && next()
+    // socket.on('offers', function(data) {
+    //     // send offers to everyone in a given room
+    //     Object.keys(clients).forEach(function(clientId, i) {
+    //         var client = clients[clientId]
+    //         if (client !== socket) {
+    //             var offerObj = data.offers[i]
+    //             var emittedOffer = {
+    //                 fromPeerId: socket.id,
+    //                 offerId: offerObj.offerId,
+    //                 offer: offerObj.offer
+    //             }
+
+    //             client.emit('offer', emittedOffer)
+
+    //             // debug('Emitting offer: %s', JSON.stringify(emittedOffer))
+    //             console.log('self offer [%s]', JSON.stringify(emittedOffer))
+    //         }
+    //     })
+    // })
+
+    // socket.on('peer-signal', function(data) {
+    //     var toPeerId = data.toPeerId
+    //     var client = clients[toPeerId]
+    //     client.emit('peer-signal', data)
+
+    //     // debug('Signal peer id %s', toPeerId);
+    //     console.log('self signal to peer id [%s]', toPeerId)
+    // })
+
+    typeof next === 'function' && next()
 }
